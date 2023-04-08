131a326a6669b11ea5048148dc6bac8cb301ebc9
YunSeok-Choi <dbstjr9898@naver.com>
2023-04-09 04:32:43 +0900

    :recycle: Refactor: ContentView Braille reafactoring

--- Brailling.swiftpm/ContentView.swift
+++ Brailling.swiftpm/ContentView.swift
@@ -21,7 +21,7 @@ struct ContentView: View {
                     LView.frame(width: 80)
                     
                     Text("ing")
-                        .font(.system(size: 157, design: .rounded ))
+                        .font(.system(size: 157, design: .rounded))
                         .tracking(10)
                 }
                 
@@ -50,74 +50,46 @@ struct ContentView: View {
     var LView: some View {
         HStack {
             VStack {
-                Circle()
-                    .fill(.black)
-                    .shadow(color: Color.black.opacity(0.2), radius: 10, x: -5, y: -5)
-                    .shadow(color: Color.white.opacity(0.7), radius: 10, x: 10, y: 10)
+                BlackBraillie
                 
-                Circle()
-                    .fill(.black)
-                    .shadow(color: Color.black.opacity(0.2), radius: 10, x: -5, y: -5)
-                    .shadow(color: Color.white.opacity(0.7), radius: 10, x: 10, y: 10)
+                BlackBraillie
                 
-                Circle()
-                    .fill(.black)
-                    .shadow(color: Color.black.opacity(0.2), radius: 10, x: -5, y: -5)
-                    .shadow(color: Color.white.opacity(0.7), radius: 10, x: 10, y: 10)
+                BlackBraillie
             }
             VStack {
-                Circle()
-                    .fill(.white)
-                    .overlay(
-                        Circle()
-                            .stroke(Color.gray, lineWidth: 4)
-                            .blur(radius: 4)
-                            .offset(x: 2, y: 2)
-                            .mask(Circle().fill(LinearGradient(.black,. clear)))
-                    )
-                    .overlay(
-                        Circle()
-                            .stroke(Color.white, lineWidth: 8)
-                            .blur(radius: 4)
-                            .offset(x: -2, y: -2)
-                            .mask(Circle().fill(LinearGradient(.clear, .black)))
-                    )
+                WhiteBraiile
                 
-                Circle()
-                    .fill(.white)
-                    .overlay(
-                        Circle()
-                            .stroke(Color.gray, lineWidth: 4)
-                            .blur(radius: 4)
-                            .offset(x: 2, y: 2)
-                            .mask(Circle().fill(LinearGradient(.black,. clear)))
-                    )
-                    .overlay(
-                        Circle()
-                            .stroke(Color.white, lineWidth: 8)
-                            .blur(radius: 4)
-                            .offset(x: -2, y: -2)
-                            .mask(Circle().fill(LinearGradient(.clear, .black)))
-                    )
+                WhiteBraiile
                 
-                Circle()
-                    .fill(.white)
-                    .overlay(
-                        Circle()
-                            .stroke(Color.gray, lineWidth: 4)
-                            .blur(radius: 4)
-                            .offset(x: 2, y: 2)
-                            .mask(Circle().fill(LinearGradient(.black,. clear)))
-                    )
-                    .overlay(
-                        Circle()
-                            .stroke(Color.white, lineWidth: 8)
-                            .blur(radius: 4)
-                            .offset(x: -2, y: -2)
-                            .mask(Circle().fill(LinearGradient(.clear, .black)))
-                    )            }
+                WhiteBraiile
+            }
         }
     }
     
+    var BlackBraillie: some View {
+        Circle()
+            .fill(.black)
+            .shadow(color: Color.black.opacity(0.2), radius: 10, x: -5, y: -5)
+            .shadow(color: Color.white.opacity(0.7), radius: 10, x: 10, y: 10)
+    }
+    
+    var WhiteBraiile: some View {
+        Circle()
+            .fill(.white)
+            .overlay(
+                Circle()
+                    .stroke(Color.gray, lineWidth: 4)
+                    .blur(radius: 4)
+                    .offset(x: 2, y: 2)
+                    .mask(Circle().fill(LinearGradient(.black,. clear)))
+            )
+            .overlay(
+                Circle()
+                    .stroke(Color.white, lineWidth: 8)
+                    .blur(radius: 4)
+                    .offset(x: -2, y: -2)
+                    .mask(Circle().fill(LinearGradient(.clear, .black)))
+            )
+    }
 }
 
