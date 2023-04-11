ddb6bc0fce888033c42d91ba042ae106f7da6579
YunSeok-Choi <dbstjr9898@naver.com>
2023-04-11 21:08:31 +0900

    :bug: Fix: Alphabet game in document

--- Brailling.swiftpm/GameManager.swift
+++ Brailling.swiftpm/GameManager.swift
@@ -10,14 +10,14 @@ import Foundation
 class GameManager: ObservableObject {
     
     let soundManager = SoundManager.sound
-    @Published var alphabetRandomIndex = Int.random(in: 0..<alphabetData.count)
+    @Published var alphabetRandomIndex = Int.random(in: 0..<26)
     @Published var answer: [Bool] = [false, false, false, false, false, false]
     @Published var doneCheck = false
     @Published var isWordGame = true
     @Published var wordRandomIndex = Int.random(in: 0..<wordData.count)
     
     func alphabetShuffle() {
-        alphabetRandomIndex = Int.random(in: 0..<alphabetData.count)
+        alphabetRandomIndex = Int.random(in: 0..<26)
     }
 
     func wordShuffle() {
